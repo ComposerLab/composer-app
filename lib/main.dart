2c1de7391758661fb706d86f912c45a4281872f1
gypark <gypark@charzin.com>
2022-11-04 18:28:31 +0900

    완성

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,100 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-void main() {
-  runApp(const ComposerApp());
+import 'package:composer_app/widgets/AppBar.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (kReleaseMode) {
+    await dotenv.load(fileName: "assets/.env");
+  } else {
+    await dotenv.load(fileName: "assets/.env.dev");
+  }
+
+  if (Platform.isAndroid) {
+    await AndroidInAppWebViewController.setWebContentsDebuggingEnabled(true);
+  }
+
+  runApp(
+      MaterialApp(debugShowCheckedModeBanner: false, home: new ComposerApp()));
 }
 
-class ComposerApp extends StatelessWidget {
-  const ComposerApp({super.key});
+class ComposerApp extends StatefulWidget {
+  @override
+  _ComposerAppState createState() => new _ComposerAppState();
+}
+
+class _ComposerAppState extends State<ComposerApp> {
+  final GlobalKey webViewKey = GlobalKey();
+
+  InAppWebViewController? webViewController;
+
+  InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
+      crossPlatform: InAppWebViewOptions(
+        useShouldOverrideUrlLoading: true,
+        mediaPlaybackRequiresUserGesture: false,
+        supportZoom: false,
+      ),
+      android: AndroidInAppWebViewOptions(
+        useHybridComposition: true,
+      ),
+      ios: IOSInAppWebViewOptions(
+        allowsInlineMediaPlayback: true,
+      ));
+
+  String url = dotenv.get('URL');
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hello Composer',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Composer'),
-        ),
-        body: const Center(
-          child: Text('Hello Composer'),
-        ),
-      ),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Hello Composer',
+        home: Scaffold(
+          backgroundColor: Colors.white,
+          appBar: EmptyAppBar(),
+          body: Center(
+            child: InAppWebView(
+              key: webViewKey,
+              initialUrlRequest: URLRequest(url: Uri.parse(url)),
+              initialOptions: options,
+              onWebViewCreated: (controller) {
+                webViewController = controller;
+              },
+              onLoadStart: (controller, url) {},
+              androidOnPermissionRequest:
+                  (controller, origin, resources) async {
+                return PermissionRequestResponse(
+                    resources: resources,
+                    action: PermissionRequestResponseAction.GRANT);
+              },
+              shouldOverrideUrlLoading: (controller, navigationAction) async {
+                var uri = navigationAction.request.url!;
+
+                return NavigationActionPolicy.ALLOW;
+              },
+              onLoadStop: (controller, url) async {},
+              onLoadError: (controller, url, code, message) {},
+              onProgressChanged: (controller, progress) {},
+              onUpdateVisitedHistory: (controller, url, androidIsReload) {},
+              onConsoleMessage: (controller, consoleMessage) {
+                print(consoleMessage);
+              },
+            ),
+          ),
+        ));
   }
 }
